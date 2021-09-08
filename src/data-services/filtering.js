d0c72b221d98988cf8419866d15547d82103c0c0
rod-glover <rod.glover.ca@gmail.com>
2021-09-08 11:42:29 -0700

    Add station filtering functions

---
+++ src/data-services/filtering.js
@@ -0,0 +1,50 @@
+// This module provides functions for filtering (metadata) from the data
+// services.
+
+import flow from 'lodash/fp/flow';
+import split from 'lodash/fp/split';
+import map from 'lodash/fp/map';
+import compact from 'lodash/fp/compact';
+import every from 'lodash/fp/every';
+import get from 'lodash/fp/get';
+
+// Station filtering: This is done (at the moment) client-side.
+//
+// Station filters are specified with the environment variable
+// `REACT_APP_STATION_FILTERS`, which contains zero or more semicolon-separated
+// station filter expressions. A station filter expression takes the form
+// `<path> <op> <value>`, where
+// <path> is a dot-separated path addressing a property in a station object,
+// <op> is a comparison operator (only = supported at the moment),
+// <value> is any string not containing a semicolon.
+//
+// A more flexible (but less safe) alternative would be to allow any filter
+// expression to be any valid JS expression, and to evaluate it in the context
+// of `station` using `Function`.
+
+const filterExpressionPattern = /(?<path>\w[\w.]*)(?<op>=)(?<value>.*)/;
+
+// Converts a string to an array of parsed station filter expressions.
+// Any invalid expressions are flagged on the console and otherwise ignored.
+// A parsed filter expression is an object with keys 'path`, 'op', 'value'.
+export const stationFilterExpressionsParser = flow(
+  split(';'),
+  compact,
+  map(s => [s, s.match(filterExpressionPattern)]),
+  map(([s, match]) => {
+    if (!match) {
+      console.warn(`Warning: '${s}' is not a valid station filter expression`)
+    }
+    return match;
+  }),
+  compact,
+  map(match => match.groups),
+);
+
+// Given an array of parsed filter expressions, returns a predicate that
+// evaluates the conjunction of the expressions for a given station.
+// A parsed filter expression is an object with keys 'path`, 'op', 'value'.
+export const stationFilterPredicate = expressions => station =>
+  every(
+    expression => get(expression.path, station) === expression.value
+  )(expressions);
---
+++ src/data-services/filtering.test.js
@@ -0,0 +1,85 @@
+import {
+  stationFilterExpressionsParser,
+  stationFilterPredicate,
+} from './filtering';
+import every from 'lodash/fp/every';
+import flow from 'lodash/fp/flow';
+import zipAll from 'lodash/fp/zipAll';
+import isMatch from 'lodash/fp/isMatch';
+
+
+describe('stationFilterExpressionsParser', () => {
+  it.each([
+    ['x=foo', [{ path: 'x', op: '=', value: 'foo' }]],
+    ['x.y=foo', [{ path: 'x.y', op: '=', value: 'foo' }]],
+    [
+      'x=foo;y=bar',
+      [
+        { path: 'x', op: '=', value: 'foo' },
+        { path: 'y', op: '=', value: 'bar' },
+      ]
+    ],
+    ['x=foo;blork', [{ path: 'x', op: '=', value: 'foo' }]],
+    ['x=foo;y<17', [{ path: 'x', op: '=', value: 'foo' }]],
+  ])('works for %s', (string, expected) => {
+    const expressions = stationFilterExpressionsParser(string);
+    expect(expressions.length === expected.length);
+    expect(
+      flow(
+        zipAll,
+        every(([e, x]) => isMatch(e, x)),
+      )([expressions, expected])
+    ).toBe(true);
+  });
+});
+
+
+describe('stationFilterPredicate', () => {
+  describe.each([
+    [
+      // expressions
+      [{ path: 'x', op: '=', value: 'foo' }],
+      // trials
+      [
+        [{ x: 'foo' }, true],
+        [{ x: 'foo', y: 'bar' }, true],
+        [{ x: 'yow' }, false],
+        [{ y: 'foo' }, false],
+      ],
+    ],
+
+    [
+      // expressions
+      [{ path: 'x.a', op: '=', value: 'foo' }],
+      // trials
+      [
+        [{ x: 'foo' }, false],
+        [{ x: { a: 'foo' } }, true],
+        [{ x: { a: 'foo' }, y: 'bar' }, true],
+        [{ x: { a: 'yow' }  }, false],
+        [{ y: { a: 'foo' }  }, false],
+      ],
+    ],
+
+    [
+      // expressions
+      [
+        { path: 'x', op: '=', value: 'foo' },
+        { path: 'y', op: '=', value: 'bar' }
+      ],
+      // trials
+      [
+        [{ x: 'foo' }, false],
+        [{ x: 'foo', y: 'bar' }, true],
+        [{ x: 'foo', y: 'baz' }, false],
+        [{ x: 'baz', y: 'bar' }, false],
+      ],
+    ],
+
+  ])('for expressions %p', (expressions, trials) => {
+    const predicate = stationFilterPredicate(expressions);
+    it.each(trials)('works for %p', (station, expected) => {
+      expect(predicate(station)).toBe(expected);
+    });
+  });
+});
