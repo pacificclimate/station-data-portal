536b98a2d26b3444a95d116a0a7e4d056c78fb7f
rod-glover <rod.glover.ca@gmail.com>
2021-12-20 15:07:58 -0800

    Modify date filtering

--- src/utils/portals-common/portals-common.js
+++ src/utils/portals-common/portals-common.js
@@ -1,5 +1,5 @@
 import contains from 'lodash/fp/contains';
-import every from 'lodash/fp/every';
+import compact from 'lodash/fp/compact';
 import filter from 'lodash/fp/filter';
 import flatten from 'lodash/fp/flatten';
 import flow from 'lodash/fp/flow';
@@ -7,6 +7,9 @@ import intersection from 'lodash/fp/intersection';
 import map from 'lodash/fp/map';
 import some from 'lodash/fp/some';
 import uniq from 'lodash/fp/uniq';
+import isNil from 'lodash/fp/isNil';
+import union from 'lodash/fp/union';
+import reduce from 'lodash/fp/reduce';
 import { isPointInPolygonWn } from '../geometry-algorithms';
 
 
@@ -21,13 +24,110 @@ const getY = point => point[1];
 export const isPointInGeoJSONPolygon = isPointInPolygonWn(getX, getY);
 
 
+export const utcDateOnly = d => {
+  // Return a new Date object which is suitable for comparing UTC date only
+  // (fixed zero time component). One might suppose that the comparisons in
+  // the filter below also would work with local time (Date.setHours()), but
+  // they don't; only UTC works.
+  if (!d) {
+    return d;
+  }
+  const r = new Date(d);
+  r.setUTCHours(0, 0, 0, 0);
+  return r;
+};
+
+
+export const isNilOrAscending = (a, b, c) => {
+  // Allowing for nils (undefined, null), check that a <= b <= c.
+  return isNil(b) || ((isNil(a) || a <= b) && (isNil(c) || b <= c));
+};
+
+
+export const historyDateMatch = (
+  history, startDate, endDate, strict = true
+)  => {
+  // Return true if history observation dates match start and end date.
+  // Compare using dates only.
+  const minObsDate = utcDateOnly(history.min_obs_time);
+  const maxObsDate = utcDateOnly(history.max_obs_time);
+  startDate = utcDateOnly(startDate);
+  endDate = utcDateOnly(endDate);
+
+  if (strict) {
+    // Allowing for nils, check that
+    // minObsDate <= startDate <= endDate <= maxObsDate.
+    return (
+      isNilOrAscending(startDate, endDate)
+      && isNilOrAscending(minObsDate, startDate, maxObsDate)
+      && isNilOrAscending(minObsDate, endDate, maxObsDate)
+    );
+  }
+
+  // This is a variant of the looser legacy comparison. Allows nil start, end.
+  return (
+    !isNil(minObsDate) && !isNil(maxObsDate)
+    && isNilOrAscending(startDate, maxObsDate)
+    && isNilOrAscending(minObsDate, endDate)
+  );
+
+  // This is the looser comparison used by the legacy PDP portal.
+  // return (
+  //   !isNil(minObsDate) && !isNil(maxObsDate)
+  //   && !isNil(startDate) && !isNil(endDate)
+  //   && maxObsDate > startDate && minObsDate < endDate
+  // );
+};
+
+
+export const stationDateMatch = (
+  station, startDate, endDate, strict = true
+) => {
+  // TODO: Coalesce adjacent histories from a station. NB: tricky.
+  //  If we don't do this, and we use strict date matching, then a station with
+  //  several histories fully covering an interval will not be selected, even
+  //  though it should be. The question of what "adjacent" means is a bit tricky
+  //  ... would depend in part on history.freq to distinguish too-large gaps,
+  //  but we already know that attribute isn't always an accurate reflection of
+  //  actual observations.
+  return flow(
+    map(hx => historyDateMatch(hx, startDate, endDate, strict)),
+    some(Boolean),
+  )(station.histories)
+};
+
+
+export const unionAll = reduce(union, []);
+
+export const atLeastOne = items => items.length > 0;
+
+
+export const stationReportsSomeVariables = (station, variableUris) => {
+  return flow(
+    map("variable_uris"),
+    compact,
+    unionAll,
+    intersection(variableUris),
+    atLeastOne,
+  )(station.histories);
+};
+
+
+export const stationReportsAnyFreqs = (station, freqs) => {
+  return flow(
+    map("freq"),
+    compact,
+    intersection(freqs),
+    atLeastOne,
+  )(station.histories);
+};
+
+
 export const stationFilter = (
   startDate, endDate, selectedNetworks, selectedVariables, selectedFrequencies,
   onlyWithClimatology, area, allNetworks, allVariables, allStations
 ) => {
   // console.log('filteredStations allStations', allStations)
-  // console.log('filteredStations allVariables', this.state.allVariables)
-  console.log('test date', new Date())
   const selectedVariableUris = flow(
     map(selectedVariable => selectedVariable.contexts),
     flatten,
@@ -35,57 +135,30 @@ export const stationFilter = (
     uniq,
   )(selectedVariables);
   // console.log('filteredStations selectedVariableUris', selectedVariableUris)
+
   const selectedFrequencyValues =
     map(option => option.value)(selectedFrequencies);
   // console.log('filteredStations selectedVariableUris', selectedVariableUris)
-  return flow(
+
+  console.group("stationFilter")
+  const r = flow(
+    // tap(s => console.log("all stations", s?.length)),
+
     filter(station => {
       return (
-        // Filter for selected date range covered entirely by station
-        // records date range.
-        (
-          startDate === null ||
-          !station.min_obs_time ||  // not supposed to happen; optimistic
-          startDate >= station.min_obs_time
-        ) &&
-        (
-          endDate === null ||
-          !station.max_obs_time ||  // observations to current date
-          endDate <= station.max_obs_time
-        ) &&
-
-        // Filter for selected date range with any overlap at all with
-        // station records date range. Probably not wanted.
-        // (
-        //   endDate === null ||
-        //   !station.min_obs_time ||
-        //   endDate >= station.min_obs_time
-        // ) &&
-        // (
-        //   startDate === null ||
-        //   !station.max_obs_time ||
-        //   startDate <= station.max_obs_time
-        // ) &&
+        stationDateMatch(station, startDate, endDate, false)
 
         // Station is part of one of selected networks
-        contains(
+        && contains(
           station.network_uri,
           map(nw => nw.value.uri)(selectedNetworks)
-        ) &&
-
-        // Station reports one of selected variables
-        (
-          station.histories && station.histories[0] &&
-          intersection(station.histories[0].variable_uris, selectedVariableUris).length > 0
-        ) &&
-
-        // Station recording frequency is one of selected frequencies
-        (
-          station.histories && station.histories[0] &&
-          contains(station.histories[0].freq, selectedFrequencyValues)
         )
-      )
+
+        && stationReportsSomeVariables(station, selectedVariableUris)
+        && stationReportsAnyFreqs(station, selectedFrequencyValues)
+      );
     }),
+    // tap(s => console.log("after date etc filtering", s?.length)),
 
     // Stations match `onlyWithClimatology`:
     // If `onlyWithClimatology`, station reports a climatology variable.
@@ -101,6 +174,7 @@ export const stationFilter = (
         some(({ cell_method }) => /(within|over)/.test(cell_method))
       )(allVariables)
     }),
+    // tap(s => console.log("after onlyWithClimatology filtering", s?.length)),
 
     // Stations are inside `area`
     filter(station => {
@@ -115,5 +189,9 @@ export const stationFilter = (
             [station.histories[0].lon, station.histories[0].lat] ))(area.coordinates);
 
     }),
+    // tap(s => console.log("after area filtering", s?.length)),
   )(allStations);
+
+  console.groupEnd()
+  return r;
 };
