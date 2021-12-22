4d286a4a8726b19202251370b859b6c7737bbb2e
rod-glover <rod.glover.ca@gmail.com>
2021-12-22 11:07:01 -0800

    Refactor and improve StationTooltip

--- src/components/maps/StationTooltip/StationTooltip.js
+++ src/components/maps/StationTooltip/StationTooltip.js
@@ -1,21 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Tooltip } from 'react-leaflet'
+import { stationNetwork, uniqStationNames } from '../../../utils/station-info';
+import flow from 'lodash/fp/flow';
+import join from 'lodash/fp/join';
 import './StationTooltip.css';
 
-function StationTooltip({ station, network }) {
-  const histories = station.histories;
-  const history0 = histories[0];
+function StationTooltip({ station, allNetworks }) {
+  const network = stationNetwork(allNetworks, station);
+  const stationNames = flow(
+    uniqStationNames,
+    join(", "),
+  )(station);
+
   return (
     <Tooltip>
-      {history0.station_name} <span>({network.name})</span>
+      {stationNames} <span>({network.name})</span>
     </Tooltip>
   );
 }
 
 StationTooltip.propTypes = {
   station: PropTypes.object.isRequired,
-  network: PropTypes.object.isRequired,
+  allNetworks: PropTypes.array.isRequired,
 };
 
 export default StationTooltip;
