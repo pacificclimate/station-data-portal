c8b8d13e04e82d3f6c6b68491cc503add35044f0
rod-glover <rod.glover.ca@gmail.com>
2021-12-22 11:09:33 -0800

    Refactor StationMarkers; add multi-location polygon

--- src/components/maps/StationMarkers/StationMarkers.js
+++ src/components/maps/StationMarkers/StationMarkers.js
@@ -1,20 +1,19 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { CircleMarker, Polygon } from 'react-leaflet';
-import compact from 'lodash/fp/compact';
 import map from 'lodash/fp/map';
-import find from 'lodash/fp/find';
 import flow from 'lodash/fp/flow';
-import tap from 'lodash/fp/tap';
-import uniqWith from 'lodash/fp/uniqWith';
 
-import { utcDateOnly } from '../../../utils/portals-common';
 import StationTooltip from '../StationTooltip';
 import StationPopup from '../StationPopup';
 
 import logger from '../../../logger';
 
 import './StationMarkers.css';
+import {
+  stationNetwork,
+  uniqStationLocations
+} from '../../../utils/station-info';
 
 logger.configure({ active: true });
 
@@ -22,111 +21,74 @@ logger.configure({ active: true });
 const noStations = [];
 
 
-const network_for = (station, networks) => (
-  find({ uri: station.network_uri })(networks)
-);
-
-
-const variables_for = (history, variables) => (
-  flow(
-    map(variable_uri => find({ uri: variable_uri })(variables)),
-    // compacting this array should not be necessary, but the API delivers
-    // erroneous data (due ultimately to erroneous database records, I believe)
-    // that causes some of the variables to be "missing".
-    compact,
-  )(history.variable_uris)
-);
-
-
-function StationMarker({ station, allNetworks, allVariables, markerOptions }) {
-  const histories = station.histories;
-  const history = histories[0];
-  const network = network_for(station, allNetworks);
-  const variables = variables_for(history, allVariables);
+function StationMarker({
+  station, allNetworks, allVariables, markerOptions, polygonOptions
+}) {
+  const network = stationNetwork(allNetworks, station);
 
   const uniqLatLngs = flow(
-    uniqWith(
-      (hx1, hx2) => (
-        utcDateOnly(hx1.min_obs_time).getTime()
-        === utcDateOnly(hx2.min_obs_time).getTime()
-        && utcDateOnly(hx1.max_obs_time).getTime()
-        === utcDateOnly(hx2.max_obs_time).getTime()
-      )
-    ),
+    uniqStationLocations,
     map(hx => ({ lng: hx.lon, lat: hx.lat }))
-  )(station.histories);
+  )(station);
 
-  return map(
-    latLng => (
-      <CircleMarker
-        key={station.id}
-        center={latLng}
-        {...markerOptions}
-        color={network && network.color}
-      >
-        <StationTooltip
-          station={station}
-          network={network}
-        />
-        <StationPopup
-          station={station}
-          network={network}
-          variables={variables}
-        />
-      </CircleMarker>
-    ),
-    uniqLatLngs
+  const stationTooltip = (
+    <StationTooltip
+      station={station}
+      allNetworks={allNetworks}
+    />
   );
 
-
-  if (histories.length === 0) {
-    return null;
-  }
-
-  if (histories.length === 1) {
-    return (
-      <CircleMarker
-        key={station.id}
-        center={{ lng: history.lon, lat: history.lat }}
-        {...markerOptions}
-        color={network && network.color}
-      >
-        <StationTooltip
-          station={station}
-          network={network}
-        />
-        <StationPopup
-          station={station}
-          network={network}
-          variables={variables}
-        />
-      </CircleMarker>
-    );
-  }
+  const stationPopup = (
+    <StationPopup
+      station={station}
+      allNetworks={allNetworks}
+      allVariables={allVariables}
+    />
+  );
 
   return (
-    <Polygon
-      pathOptions={{ color: 'purple' }}
-      positions={uniqLatLngs}
-    >
-      <StationTooltip
-        station={station}
-        network={network}
-      />
-      <StationPopup
-        station={station}
-        network={network}
-        variables={variables}
-      />
-    </Polygon>
-  )
+    <React.Fragment>
+      {
+        map(
+          latLng => (
+            <CircleMarker
+              key={station.id}
+              center={latLng}
+              {...markerOptions}
+              color={network && network.color}
+            >
+              {stationTooltip}
+              {stationPopup}
+            </CircleMarker>
+          ),
+          uniqLatLngs
+        )
+      }
+      {
+        uniqLatLngs.length > 1 && (
+          <Polygon
+            pathOptions={polygonOptions}
+            positions={uniqLatLngs}
+          >
+            {stationTooltip}
+            {stationPopup}
+          </Polygon>
+        )
+      }
+    </React.Fragment>
+  );
 }
 
-StationMarker.propTypes = {
-  stations: PropTypes.array.isRequired,
+const commonStationMarkerPropTypes = {
   allNetworks: PropTypes.array.isRequired,
   allVariables: PropTypes.array.isRequired,
   markerOptions: PropTypes.object,
+  polygonOptions: PropTypes.object,
+};
+
+StationMarker.propTypes = {
+  station: PropTypes.object.isRequired,
+  ...commonStationMarkerPropTypes,
 };
 
 StationMarker.defaultProps = {
@@ -135,6 +97,9 @@ StationMarker.defaultProps = {
     weight: 1,
     fillOpacity: 0.75,
     color: '#000000',
+  },
+  polygonOptions: {
+    color: "purple",
   },
 };
 
@@ -154,18 +119,9 @@ function StationMarkers({
 
 StationMarkers.propTypes = {
   stations: PropTypes.array.isRequired,
-  allNetworks: PropTypes.array.isRequired,
-  allVariables: PropTypes.array.isRequired,
-  markerOptions: PropTypes.object,
+  ...commonStationMarkerPropTypes,
 };
 
-StationMarkers.defaultProps = {
-  markerOptions: {
-    radius: 4,
-    weight: 1,
-    fillOpacity: 0.75,
-    color: '#000000',
-  },
-};
+StationMarkers.defaultProps = StationMarker.defaultProps;
 
 export default StationMarkers;
