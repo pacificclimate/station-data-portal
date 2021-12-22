767dc2c534ddf08bf06e91751ed7a1deb211c420
rod-glover <rod.glover.ca@gmail.com>
2021-12-22 11:08:24 -0800

    Refactor and improve StationPopup

--- src/components/maps/StationPopup/StationPopup.js
+++ src/components/maps/StationPopup/StationPopup.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import { Table } from 'react-bootstrap';
 import { Popup } from 'react-leaflet'
 import flow from 'lodash/fp/flow';
@@ -12,9 +12,12 @@ import logger from '../../../logger';
 
 import './StationPopup.css';
 import {
+  stationNetwork,
   uniqStationFreqs,
   uniqStationLocations,
-  uniqStationNames, uniqStationObsPeriods
+  uniqStationNames,
+  uniqStationObsPeriods,
+  uniqStationVariableNames,
 } from '../../../utils/station-info';
 
 logger.configure({ active: true });
@@ -23,120 +26,125 @@ logger.configure({ active: true });
 const formatDate = d => d ? d.toISOString().substr(0,10) : 'unknown';
 
 
-class StationPopup extends Component {
-  static propTypes = {
-    station: PropTypes.object.isRequired,
-    network: PropTypes.object.isRequired,
-    variables: PropTypes.array.isRequired,
-    defaultNetworkColor: PropTypes.string,
-  };
+function StationPopup({
+  station, allNetworks, allVariables, defaultNetworkColor
+}) {
+  const network = stationNetwork(allNetworks, station);
+  const networkColor =
+    chroma(network.color ?? defaultNetworkColor).alpha(0.5).css();
 
-  static defaultProps = {
-    defaultNetworkColor:
-      process.env.REACT_APP_DEFAULT_NETWORK_COLOR ?? '#000000',
-  }
+  const stationNames = flow(
+    uniqStationNames,
+    join(", "),
+  )(station);
 
-  render() {
-    const { station, network, variables, defaultNetworkColor } = this.props;
-    const networkColor =
-      chroma(network.color ?? defaultNetworkColor).alpha(0.5).css();
+  const stationLocations = (
+    <ul className={"compact"}>
+      {
+        flow(
+          uniqStationLocations,
+          map(loc => (
+            <li>
+              {-loc.lon} W <br/>
+              {loc.lat} N <br/>
+              Elev. {loc.elevation} m
+            </li>
+          )),
+        )(station)
+      }
+    </ul>
+  );
 
+  const stationObsPeriods = (
+    <ul className={"histories"}>
+      {
+        flow(
+          uniqStationObsPeriods,
+          map(hx => (
+            <li>{formatDate(hx.min_obs_time)} to {formatDate(hx.max_obs_time)}</li>
+          ))
+        )(station)
+      }
+    </ul>
+  );
 
-    const stationNames = flow(
-      uniqStationNames,
-      join(", "),
-    )(station);
+  const stationObsFreqs = (
+    <ul className={"compact"}>
+      {
+        flow(
+          uniqStationFreqs,
+          map(freq => (<li>{FrequencySelector.valueToLabel(freq)}</li>)),
+        )(station)
+      }
+    </ul>
+  );
 
-    const stationLocations = (
-      <ul className={"compact"}>
-        {
-          flow(
-            uniqStationLocations,
-            map(loc => (
-              <li>
-                {-loc.lon} W <br/>
-                {loc.lat} N <br/>
-                Elev. {loc.elevation} m
-              </li>
-            )),
-          )(station)
-        }
-      </ul>
-    );
+  const variableNames = (
+    <ul className={"variables"}>
+      {map(
+        name => (<li>{name}</li>),
+        uniqStationVariableNames(allVariables, station)
+      )}
+    </ul>
+  );
 
-    const stationObsPeriods = (
-      <ul className={"histories"}>
-        {
-          flow(
-            uniqStationObsPeriods,
-            map(hx => (
-              <li>{formatDate(hx.min_obs_time)} to {formatDate(hx.max_obs_time)}</li>
-            ))
-          )(station)
-        }
-      </ul>
-    );
-    const stationObsFreqs = (
-      <ul className={"compact"}>
-        {
-          flow(
-            uniqStationFreqs,
-            map(freq => (<li>{FrequencySelector.valueToLabel(freq)}</li>)),
-          )(station)
-        }
-      </ul>
-    );
+  return (
+    <Popup>
+      <h1>Station: {stationNames}</h1>
+      <Table size={'sm'} condensed>
+        <tbody>
+        <tr>
+          <td>Network</td>
+          <td>
+            <span style={{backgroundColor: networkColor}}>
+              {`${network.name}`}
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <td>Native ID</td>
+          <td>{station.native_id}</td>
+        </tr>
+        <tr>
+          <td>Database ID</td>
+          <td>{station.id}</td>
+        </tr>
+        <tr>
+          <td>History count</td>
+          <td>{station.histories.length}</td>
+        </tr>
+        <tr>
+          <td>Locations</td>
+          <td>{stationLocations}</td>
+        </tr>
+        <tr>
+          <td>Record spans</td>
+          <td>{stationObsPeriods}</td>
+        </tr>
+        <tr>
+          <td>Observation freqs</td>
+          <td>{stationObsFreqs}</td>
+        </tr>
+        <tr>
+          <td>Recorded variables</td>
+          <td>{variableNames}</td>
+        </tr>
+        </tbody>
+      </Table>
+    </Popup>
+  );
+}
 
-    return (
-      <Popup>
-        <h1>Station: {stationNames}</h1>
-        <Table size={'sm'} condensed>
-          <tbody>
-          <tr>
-            <td>Network</td>
-            <td>
-              <span style={{backgroundColor: networkColor}}>
-                {`${network.name}`}
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Native ID</td>
-            <td>{station.native_id}</td>
-          </tr>
-          <tr>
-            <td>Database ID</td>
-            <td>{station.id}</td>
-          </tr>
-          <tr>
-            <td>Locations</td>
-            <td>{stationLocations}</td>
-          </tr>
-          <tr>
-            <td>Record spans</td>
-            <td>{stationObsPeriods}</td>
-          </tr>
-          <tr>
-            <td>Observation freqs</td>
-            <td>{stationObsFreqs}</td>
-          </tr>
-          <tr>
-            <td>Recorded variables</td>
-            <td>
-              <ul className={"variables"}>
-                {
-                  map(variable => (
-                    <li>{variable.display_name}</li>
-                  ))(variables)
-                }
-              </ul>
-            </td>
-          </tr>
-          </tbody>
-        </Table>
-      </Popup>
-    );
-  }
+StationPopup.propTypes = {
+  station: PropTypes.object.isRequired,
+  allNetworks: PropTypes.array.isRequired,
+  allVariables: PropTypes.array.isRequired,
+  defaultNetworkColor: PropTypes.string,
+};
+
+StationPopup.defaultProps = {
+  defaultNetworkColor:
+    process.env.REACT_APP_DEFAULT_NETWORK_COLOR ?? '#000000',
 }
 
 export default StationPopup;
