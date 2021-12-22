0487bccadd2180e7b40f196399c2400ae8cf025b
rod-glover <rod.glover.ca@gmail.com>
2021-12-21 17:24:38 -0800

    Draw a station marker for every history WIP

--- src/components/maps/StationMarkers/StationMarkers.js
+++ src/components/maps/StationMarkers/StationMarkers.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { CircleMarker } from 'react-leaflet';
+import React from 'react';
+import { CircleMarker, Polygon } from 'react-leaflet';
 import compact from 'lodash/fp/compact';
 import map from 'lodash/fp/map';
 import find from 'lodash/fp/find';
 import flow from 'lodash/fp/flow';
 import tap from 'lodash/fp/tap';
+import uniqWith from 'lodash/fp/uniqWith';
 
+import { utcDateOnly } from '../../../utils/portals-common';
 import StationTooltip from '../StationTooltip';
 import StationPopup from '../StationPopup';
 
@@ -36,57 +38,134 @@ const variables_for = (history, variables) => (
 );
 
 
-class StationMarkers extends Component {
-  static propTypes = {
-    stations: PropTypes.array.isRequired,
-    allNetworks: PropTypes.array.isRequired,
-    allVariables: PropTypes.array.isRequired,
-    markerOptions: PropTypes.object,
-  };
+function StationMarker({ station, allNetworks, allVariables, markerOptions }) {
+  const histories = station.histories;
+  const history = histories[0];
+  const network = network_for(station, allNetworks);
+  const variables = variables_for(history, allVariables);
 
-  static defaultProps = {
-    markerOptions: {
-      radius: 4,
-      weight: 1,
-      fillOpacity: 0.75,
-      color: '#000000',
-    },
-  };
+  const uniqLatLngs = flow(
+    uniqWith(
+      (hx1, hx2) => (
+        utcDateOnly(hx1.min_obs_time).getTime()
+        === utcDateOnly(hx2.min_obs_time).getTime()
+        && utcDateOnly(hx1.max_obs_time).getTime()
+        === utcDateOnly(hx2.max_obs_time).getTime()
+      )
+    ),
+    map(hx => ({ lng: hx.lon, lat: hx.lat }))
+  )(station.histories);
 
-  render() {
+  return map(
+    latLng => (
+      <CircleMarker
+        key={station.id}
+        center={latLng}
+        {...markerOptions}
+        color={network && network.color}
+      >
+        <StationTooltip
+          station={station}
+          network={network}
+        />
+        <StationPopup
+          station={station}
+          network={network}
+          variables={variables}
+        />
+      </CircleMarker>
+    ),
+    uniqLatLngs
+  );
+
+
+  if (histories.length === 0) {
+    return null;
+  }
+
+  if (histories.length === 1) {
     return (
-      flow(
-        tap(stations => console.log('stations', stations)),
-        map(station => {
-          const history = station.histories[0];
-          const network = network_for(station, this.props.allNetworks);
-          const variables = variables_for(history, this.props.allVariables);
-          return (
-            history &&
-            <CircleMarker
-              key={station.id}
-              center={{
-                lng: history.lon,
-                lat: history.lat
-              }}
-              {...this.props.markerOptions}
-              color={network && network.color}
-            >
-              <StationTooltip
-                station={station}
-                network={network}
-              />
-              <StationPopup
-                station={station}
-                network={network}
-                variables={variables}
-              />
-            </CircleMarker>
-          )
-        })
-      )(this.props.stations || noStations)
+      <CircleMarker
+        key={station.id}
+        center={{ lng: history.lon, lat: history.lat }}
+        {...markerOptions}
+        color={network && network.color}
+      >
+        <StationTooltip
+          station={station}
+          network={network}
+        />
+        <StationPopup
+          station={station}
+          network={network}
+          variables={variables}
+        />
+      </CircleMarker>
     );
   }
+
+  return (
+    <Polygon
+      pathOptions={{ color: 'purple' }}
+      positions={uniqLatLngs}
+    >
+      <StationTooltip
+        station={station}
+        network={network}
+      />
+      <StationPopup
+        station={station}
+        network={network}
+        variables={variables}
+      />
+    </Polygon>
+  )
 }
+
+StationMarker.propTypes = {
+  stations: PropTypes.array.isRequired,
+  allNetworks: PropTypes.array.isRequired,
+  allVariables: PropTypes.array.isRequired,
+  markerOptions: PropTypes.object,
+};
+
+StationMarker.defaultProps = {
+  markerOptions: {
+    radius: 4,
+    weight: 1,
+    fillOpacity: 0.75,
+    color: '#000000',
+  },
+};
+
+
+function StationMarkers({
+  stations, ...rest
+}) {
+  return (
+    map(
+      station => (
+        <StationMarker station={station} {...rest}/>
+      ),
+      stations || noStations
+    )
+  );
+}
+
+StationMarkers.propTypes = {
+  stations: PropTypes.array.isRequired,
+  allNetworks: PropTypes.array.isRequired,
+  allVariables: PropTypes.array.isRequired,
+  markerOptions: PropTypes.object,
+};
+
+StationMarkers.defaultProps = {
+  markerOptions: {
+    radius: 4,
+    weight: 1,
+    fillOpacity: 0.75,
+    color: '#000000',
+  },
+};
 
 export default StationMarkers;
