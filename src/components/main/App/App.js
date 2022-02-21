3771a4f18125cacdf97d3b5592d90d04a3a4ec74
rod-glover <rod.glover.ca@gmail.com>
2022-02-21 15:15:15 -0800

    Remove Ver B and all nav from App.js

--- src/components/main/App/App.js
+++ src/components/main/App/App.js
@@ -1,65 +1,18 @@
-import React, {Component} from 'react';
+import React  from 'react';
 import { Grid } from 'react-bootstrap';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import { LinkContainer } from 'react-router-bootstrap';
-import url from 'url';
-import urjJoin from 'url-join';
 
 import logger from '../../../logger';
 import Header from '../Header/Header';
 
 import VersionA from '../VersionA';
-import VersionB from '../VersionB';
 
 import './App.css';
 
-const navSpec = [
-  { label: 'Version A', path: 'A', component: VersionA },
-  { label: 'Version B', path: 'B', component: VersionB },
-];
-
-
-export default class App extends Component {
-    render() {
-      return (
-        <Grid fluid className="App">
-          <Header/>
-          <VersionA/>
-        </Grid>
-      );
-      const basePath = url.parse(process.env.PUBLIC_URL).pathname || '';
-      return (
-        <Router basename={urjJoin(basePath, '#')}>
-          <div>
-            <Navbar fluid>
-              <Nav>
-                {
-                  navSpec.map(({label, path}) => (
-                    <LinkContainer to={`/${path}`}>
-                      <NavItem eventKey={path}>
-                        {label}
-                      </NavItem>
-                    </LinkContainer>
-                  ))
-                }
-              </Nav>
-            </Navbar>
-
-            <Grid fluid className="App">
-                <Header/>
-                <Switch>
-                  {
-                    navSpec.map(({path, component}) => (
-                      <Route path={`/${path}`} component={component}/>
-                    ))
-                  }
-                  <Redirect to={'/A'}/>
-                </Switch>
-            </Grid>
-          </div>
-        </Router>
-        );
-    }
+export default function App() {
+    return (
+      <Grid fluid className="App">
+        <Header/>
+        <VersionA/>
+      </Grid>
+    );
 }
