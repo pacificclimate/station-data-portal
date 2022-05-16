680de0ba30d8616ecf4bb63992058cd43249629d
rod-glover <rod.glover.ca@gmail.com>
2022-05-16 14:56:21 -0700

    Configure disclaimer from env vars

--- src/components/info/Disclaimer/Disclaimer.js
+++ src/components/info/Disclaimer/Disclaimer.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import './Disclaimer.css';
+import { configBool, configString } from '../../../utils/configuration';
+
+
+const disclaimerEnabled = configBool("DISCLAIMER_ENABLED")
+const disclaimerTitle = configString("DISCLAIMER_TITLE", "Disclaimer Title")
+const disclaimerBody = configString("DISCLAIMER_BODY", "Disclaimer body ...")
+const disclaimerButtonLabel =
+  configString("DISCLAIMER_BUTTON_LABEL", "Acknowledge")
 
 
 function Disclaimer() {
-  const [acknowledged, setAcknowledged] = useState(false);
+  const [acknowledged, setAcknowledged] = useState(!disclaimerEnabled);
   const acknowledge = () => setAcknowledged(true);
 
   return (
@@ -12,15 +20,16 @@ function Disclaimer() {
       backdrop={"static"}
       show={!acknowledged}
       onHide={acknowledge}
+      style={{ zIndex: 2000 }}
     >
       <Modal.Header>
-        <Modal.Title>
-          Disclaimer Title
-        </Modal.Title>
+        <Modal.Title>{disclaimerTitle}</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Disclaimer body</Modal.Body>
+      <Modal.Body>{disclaimerBody}</Modal.Body>
       <Modal.Footer>
-        <Button bsStyle={"primary"} onClick={acknowledge}>Acknowledge</Button>
+        <Button bsStyle={"primary"} onClick={acknowledge}>
+          {disclaimerButtonLabel}
+        </Button>
       </Modal.Footer>
     </Modal>
   );
