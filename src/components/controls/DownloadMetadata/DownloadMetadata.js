d989e75da8696278130bb032f1497b28ff0ccacc
rod-glover <rod.glover.ca@gmail.com>
2022-05-31 12:50:56 -0700

    Do synchronous csv download

--- src/components/controls/DownloadMetadata/DownloadMetadata.js
+++ src/components/controls/DownloadMetadata/DownloadMetadata.js
@@ -1,18 +1,56 @@
+// This component uses the React Table infrastructure (already present for
+// metadata display) to prepare the data for React CSV package, which creates
+// a CSV file.
 import PropTypes from 'prop-types';
 import React from 'react';
+import flow from 'lodash/fp/flow';
+import map from 'lodash/fp/map';
+import { useTable } from 'react-table';
+import { CSVLink } from 'react-csv';
 
 import logger from '../../../logger';
 
 import './DownloadMetadata.css';
-import { Button } from 'react-bootstrap';
 
 logger.configure({ active: true });
 
 function DownloadMetadata({ data, columns }) {
+  const {
+    allColumns,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  console.log("### allColumns", allColumns)
+  const csvHeaders = map("Header", allColumns);
+  console.log("### csvHeaders", csvHeaders)
+
+  // Convert each row to an array of values matching header array.
+  const csvData = () => flow(
+    map(row => {
+      prepareRow(row);
+      return row.values;
+    }),
+    map(values =>
+      map(column => {
+        const value = values[column.id];
+        return column.csv ?  column.csv(value) : value;
+      }, allColumns)
+    ),
+  )(rows);
+
+  if (rows.length === 0) {
+    return null;
+  }
+
   return (
-    <Button disabled title={"This function is not yet implemented"}>
+    <CSVLink
+      headers={csvHeaders}
+      // data={csvData}
+      data={csvData()}
+    >
       Download Metadata
-    </Button>
+    </CSVLink>
   );
 }
 
