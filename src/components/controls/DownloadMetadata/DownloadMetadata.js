a656b69931d23888bbff2f88644cca92ad3f7148
rod-glover <rod.glover.ca@gmail.com>
2022-05-31 15:07:10 -0700

    Do lazy csv download

--- src/components/controls/DownloadMetadata/DownloadMetadata.js
+++ src/components/controls/DownloadMetadata/DownloadMetadata.js
@@ -2,9 +2,10 @@
 // metadata display) to prepare the data for React CSV package, which creates
 // a CSV file.
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import flow from 'lodash/fp/flow';
 import map from 'lodash/fp/map';
+import tap from 'lodash/fp/tap';
 import { useTable } from 'react-table';
 import { CSVLink } from 'react-csv';
 
@@ -20,13 +21,14 @@ function DownloadMetadata({ data, columns }) {
     rows,
     prepareRow,
   } = useTable({ columns, data });
-
+  const [csvData, setCsvData] = useState([]);
   console.log("### allColumns", allColumns)
   const csvHeaders = map("Header", allColumns);
   console.log("### csvHeaders", csvHeaders)
 
   // Convert each row to an array of values matching header array.
-  const csvData = () => flow(
+  const makeCsvData = () => flow(
+    tap(() => console.log("### makeCsvData called")),
     map(row => {
       prepareRow(row);
       return row.values;
@@ -39,15 +41,18 @@ function DownloadMetadata({ data, columns }) {
     ),
   )(rows);
 
-  if (rows.length === 0) {
-    return null;
+  const handleClick = (event, done) => {
+    setCsvData(makeCsvData());
+    done(true);
   }
 
   return (
     <CSVLink
       headers={csvHeaders}
-      // data={csvData}
-      data={csvData()}
+      // data={makeCsvData}
+      data={csvData}
+      asyncOnClick={true}
+      onClick={handleClick}
     >
       Download Metadata
     </CSVLink>
