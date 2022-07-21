f907d1093d85df51ce06a9e5c7c057e5b11a0341
rod-glover <rod.glover.ca@gmail.com>
2022-07-21 09:17:43 -0700

    Clean up code

--- src/components/info/StationData/StationData.js
+++ src/components/info/StationData/StationData.js
@@ -57,58 +57,49 @@ function StationData({
                     dataCategory, clipToDate, fileFormat
                   });
                   const linkLabel = `Download ${capitalize(dataCategory)}`;
-                  const urlTooLong = downloadUrl.length <= config.maxUrlLength;
-                  return urlTooLong ? (
-                    <a
-                      href={downloadUrl}
-                      download={dataDownloadFilename({
-                        dataCategory, fileFormat
-                      })}
-                      className="btn btn-primary btn-sm me-2"
-                      key={dataCategory}
-                    >
-                      {linkLabel}
-                    </a>
-                  ) : (
+                  const urlTooLong = downloadUrl.length > config.maxUrlLength;
+                  return (
                     <>
                       <Button
                         variant={"primary"}
                         size={"sm"}
                         className={"me-2"}
-                        disabled
                         key={dataCategory}
+                        disabled={urlTooLong}
+                        href={urlTooLong ? undefined : downloadUrl}
+                        download={urlTooLong ? undefined : dataDownloadFilename(
+                          { dataCategory, fileFormat }
+                        )}
                       >
                         {linkLabel}
                       </Button>
-                      <span className={"me-2"}>
-                      <InfoPopup
-                        title={"Download button disabled"}
-                      >
-                        <p>
-                          This button is disabled because the length of the URL
-                          for the download ({downloadUrl.length}) exceeds the
-                          maximum allowable length for URLs
-                          ({config.maxUrlLength}).
-                        </p>
-                        <p>
-                          This is a technical issue, which will be fixed in an
-                          upcoming release. In the meantime, you can try the
-                          following:
-                        </p>
-                        <p>
-                          The most likely cause of this problem is that you
-                          selected a subset of Variables to download. Please
-                          try selecting either fewer Variables, or all (use
-                          the **All** button).
-                        </p>
-                        <p>
-                          You can also try the same strategy with the Networks
-                          and/or Frequencies selectors, but since they have
-                          many fewer options, they are less likely to be either
-                          the cause or the solution.
-                        </p>
-                      </InfoPopup>
-                      </span>
+                      { urlTooLong && (<span className={"me-2"}>
+                        <InfoPopup title={"Download button disabled"}>
+                          <p>
+                            This button is disabled because the length of the
+                            URL for the download ({downloadUrl.length}) exceeds
+                            the maximum allowable length for URLs
+                            ({config.maxUrlLength}).
+                          </p>
+                          <p>
+                            This is a technical issue, which will be fixed in an
+                            upcoming release. In the meantime, you can do the
+                            following:
+                          </p>
+                          <p>
+                            The most likely cause of this problem is that you
+                            selected a subset of Variables to download. Please
+                            try selecting either fewer Variables, or all (use
+                            the **All** button).
+                          </p>
+                          <p>
+                            You can also try the same strategy with the Networks
+                            and/or Frequencies selectors, but since they have
+                            many fewer options, they are less likely to be
+                            either the cause or the solution.
+                          </p>
+                        </InfoPopup>
+                      </span>)}
                     </>
                   );
                 },
