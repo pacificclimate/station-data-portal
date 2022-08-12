0c634123236e88079b6788cf4d30dff5cc11899c
rod-glover <rod.glover.ca@gmail.com>
2022-08-12 11:10:02 -0700

    Code cleanup

--- src/components/controls/IncludeStationsWithNoObsControl/IncludeStationsWithNoObsControl.js
+++ src/components/controls/IncludeStationsWithNoObsControl/IncludeStationsWithNoObsControl.js
@@ -17,25 +17,3 @@ IncludeStationsWithNoObsControl.propTypes = {
   value: PropTypes.bool.isRequired,
   onChange: PropTypes.func.isRequired,
 };
-
-// class OnlyWithClimatologyControl extends Component {
-//   static propTypes = {
-//     value: PropTypes.bool.isRequired,
-//     onChange: PropTypes.func.isRequired,
-//   };
-//
-//   render() {
-//     const { value, ...rest } = this.props;
-//     return (
-//       <Form>
-//         <Form.Check
-//           className={"fw-bold"}
-//           label={"Only include stations with climatology"}
-//           checked={value}
-//           {...rest}
-//         />
-//       </Form>
-//     );
-//   }
-// }
-
