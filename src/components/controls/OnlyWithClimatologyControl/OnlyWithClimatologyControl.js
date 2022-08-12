7bda3c5d02ce53f03fbbad358f1946f60b6561ea
rod-glover <rod.glover.ca@gmail.com>
2022-08-12 10:18:50 -0700

    Convert OnlyWithClimatologyControl to CbControl

--- src/components/controls/OnlyWithClimatologyControl/OnlyWithClimatologyControl.js
+++ src/components/controls/OnlyWithClimatologyControl/OnlyWithClimatologyControl.js
@@ -1,27 +1,19 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import { Form } from 'react-bootstrap';
+import React from 'react';
 
+import CheckboxControl from "../CheckboxControl";
 import './OnlyWithClimatologyControl.css';
 
-export default class OnlyWithClimatologyControl extends Component {
-  static propTypes = {
-    value: PropTypes.bool.isRequired,
-    onChange: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { value, ...rest } = this.props;
-    return (
-      <Form>
-        <Form.Check
-          className={"fw-bold"}
-          label={"Only include stations with climatology"}
-          checked={value}
-          {...rest}
-        />
-      </Form>
-    );
-  }
+export default function OnlyWithClimatologyControl(props) {
+  return (
+    <CheckboxControl
+      label={"Only include stations with climatology"}
+      {...props}
+    />
+  )
 }
 
+OnlyWithClimatologyControl.propTypes = {
+  value: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
