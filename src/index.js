78285318043ffb2caada923e5115b01d1a36b3a4
Lee Zeman <lzeman@uvic.ca>
2024-01-22 19:26:22 -0800

    add in-browser routing with react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "leaflet-draw/dist/leaflet.draw.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { createRoot } from "react-dom/client";
 
@@ -8,9 +9,24 @@ import "./bootstrap-extension.css";
 import "./index.css";
 
 import App from "./components/main/App";
+import StationPreview, {
+  loader as stationLoader,
+} from "./components/preview/StationPreview";
 import registerServiceWorker from "./registerServiceWorker";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "preview/:stationId",
+    element: <StationPreview />,
+    loader: stationLoader,
+  },
+]);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
 registerServiceWorker();
