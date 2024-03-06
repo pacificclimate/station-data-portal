342454a865416cfb071d7b6172de2e230386851e
John <johnsampson@uvic.ca>
2024-03-06 00:45:07 +0000

    #178 Add some user documentation for preview

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -8,6 +8,9 @@ echo 'alias ll="ls -alF"' >> $HOME/.bashrc
 echo 'alias la="ls -A"' >> $HOME/.bashrc
 echo 'alias l="ls -CF"' >> $HOME/.bashrc
 
+## used for user documentation
+pip install docutils
+
 # Convenience workspace directory for later use
 WORKSPACE_DIR=$(pwd)
 
