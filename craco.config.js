5f09f887f6dba9f5a50799795cfa449e781a1e6f
John <johnsampson@uvic.ca>
2024-08-28 00:00:46 +0000

    Fix warnings via package updates, ignores. Update default url config to match local settings.

--- craco.config.js
+++ craco.config.js
@@ -3,10 +3,25 @@ const { CracoAliasPlugin } = require("react-app-alias");
 const options = {}; // default is empty for most cases
 
 module.exports = {
+  eslint: null,
   plugins: [
     {
       plugin: CracoAliasPlugin,
       options: {},
     },
   ],
+  webpack: {
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes("node_modules") &&
+            warning.details &&
+            warning.details.includes("source-map-loader")
+          );
+        },
+      ],
+    },
+  },
 };
--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 window.env = {
-  PUBLIC_URL: "http://localhost:30502",
+  PUBLIC_URL: "http://localhost:3000",
   REACT_APP_BC_BASE_MAP_TILES_URL:
     "https://swarm.pacificclimate.org/tiles/bc-albers-lite/{z}/{x}/{y}.png",
   REACT_APP_YNWT_BASE_MAP_TILES_URL:
