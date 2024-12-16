651b941066c1dd832db5ec6aa51a6280c41635cb
John <johnsampson@uvic.ca>
2024-12-16 22:17:22 +0000

    Improve documentation. Use react app version from build time variable.

--- src/state/query-hooks/use-config.js
+++ src/state/query-hooks/use-config.js
@@ -95,7 +95,7 @@ const fetchConfig = async () => {
   checkMissingKeys(config);
 
   // Extend config with some env var values
-  config.appVersion = window.env.REACT_APP_APP_VERSION ?? "unknown";
+  config.appVersion = process.env.REACT_APP_APP_VERSION ?? "unknown";
 
   // Extend config with some computed goodies
   // TODO: Store shouldn't know about data presentation
